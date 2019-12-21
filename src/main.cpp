0162131a5b6fb00a43c76ebc0d6d7ddcb4dd6527
paulpatault <p.patault@gmail.com>
2019-12-21 02:37:38 +0100

    main.cpp comments

--- src/main.cpp
+++ src/main.cpp
@@ -31,25 +31,33 @@ int main()
 {
     int selected = -1;
 
+    /// Instance of the class Render that manages the window
     screen::Render window;
 
+    /// ImGui context
     screen::c_ImGui::init(window.render);
 
+    /// Vertex Array
     GLuint v_ArrayID;
     data::VAO m_VAO(v_ArrayID);
 
+    /// Set Program ID
     GLuint programID = data::Shaders::LoadShaders(
             "../resources/shaders/SimpleVertexShader.glsl",
             "../resources/shaders/SimpleFragmentShader.glsl");
 
+    /// Model View Projection Location
     screen::MVP::setLocation(programID);
 
+    /// Instance of the class LoadData that manages the .csv
     data::LoadData myData("../resources/data/rankspts.csv");
 
+    /// Vector of 20 VBOs
+    /// Usable VBOs in t_VBO and t_VBO_0 saving
     std::vector<data::VBO> t_VBO_0 = makeVBO(myData);
-
     std::vector<data::VBO> t_VBO = t_VBO_0;
 
+    /// Main loop
     do
     {
         screen::Display::clear();
